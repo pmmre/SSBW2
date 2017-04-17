63101479a5bcbdca4b5cbbc5277f51e7b2d4038a
Pablo Martín-Moreno Ruiz <pmmr@correo.ugr.es>
2017-04-17 15:21:33 +0200

    Subiendo proyecto

---
+++ pruebas/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+from pruebas.models import Category, Page
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+# Update the registeration to include this customised interface
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page)
---
+++ restaurantes/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from mongoengine import *
+
+# Conectarse a la BD
+connect('test', host='192.168.127.129', port=27017)
+
+
+class addr(EmbeddedDocument):
+    building = StringField()
+    street   = StringField()
+    city     = StringField()   # añadido
+    zipcode  = IntField()
+    coord    = GeoPointField() # OJO, al BD de test estan a revés
+                               # [long, lat] en vez de [lat, long]
+
+class likes(EmbeddedDocument):
+    grade = StringField(max_length=1)
+    score = IntField()
+    date  = DateTimeField()
+
+class restaurants(Document):
+    name             = StringField(required=True, max_length=80)
+    restaurant_id    = IntField()
+    cuisine          = StringField()
+    borough          = StringField()
+    address          = EmbeddedDocumentField(addr)              # en la misma collección
+    grades           = ListField(EmbeddedDocumentField(likes))
